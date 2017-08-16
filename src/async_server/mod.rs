3e95e63d5e2f292bcf3849386506bc2b3f57bc60
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-16 23:02:03 +0000

    Create FiniteService trait

--- src/async_server/mod.rs
+++ src/async_server/mod.rs
@@ -3,9 +3,11 @@ mod async_server;
 mod async_server_start;
 mod bound_connection_future;
 mod connection_future;
+mod finite_service;
 mod listening_async_server;
 mod errors;
 mod status;
 
 pub use self::errors::{Error, ErrorKind};
 pub use self::async_server::AsyncServer;
+pub use self::finite_service::FiniteService;
