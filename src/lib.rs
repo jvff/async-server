5747175d6c72788a5676e6575854731f8bf0f40f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-02-07 09:46:47 +0000

    Remove unnecessary first module level

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,16 @@ extern crate tokio_io;
 extern crate tokio_proto;
 extern crate tokio_service;
 
+mod active_server;
 mod async_server;
+mod bound_connection_future;
+mod connection_future;
+mod errors;
+mod finite_service;
+mod listening_server;
+mod start_server;
+mod status;
 
 pub use async_server::AsyncServer;
+pub use errors::{Error, ErrorKind};
+pub use finite_service::FiniteService;
