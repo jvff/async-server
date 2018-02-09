0b5b5689daf10a74f1852ba597d424b1e4815a75
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-02-09 22:45:56 +0000

    Use AsyncServerError in StartServer::poll()

--- src/start_server.rs
+++ src/start_server.rs
@@ -1,7 +1,7 @@
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
 
-use futures::{Async, Future, Poll, Sink, Stream};
+use futures::{Async, Future, Poll};
 use tokio_core::net::{TcpListener, TcpStream};
 use tokio_core::reactor::Handle;
 use tokio_proto::pipeline::ServerProto;
@@ -24,10 +24,6 @@ where
     P: ServerProto<TcpStream>,
     S: NewService<Request = P::Request, Response = P::Response>,
     S::Instance: FiniteService,
-    Error: From<S::Error>
-        + From<<P::Transport as Stream>::Error>
-        + From<<P::Transport as Sink>::SinkError>
-        + From<P::Error>,
 {
     pub fn new(
         address: SocketAddr,
@@ -61,16 +57,20 @@ where
         }
     }
 
-    fn start_server(&mut self) -> Poll<ListeningServer<S, P>, Error> {
+    fn start_server(
+        &mut self,
+    ) -> Poll<ListeningServer<S, P>, AsyncServerError<S::Error, P::Error>> {
         if let Some(service_factory) = self.service_factory.take() {
-            let listener = TcpListener::bind(&self.address, &self.handle)?;
+            let listener = TcpListener::bind(&self.address, &self.handle)
+                .map_err(Error::from)?;
+
             let protocol = self.protocol.clone();
 
             Ok(Async::Ready(
                 ListeningServer::new(listener, service_factory, protocol),
             ))
         } else {
-            Err(ErrorKind::AttemptToStartServerTwice.into())
+            Err(Error::from(ErrorKind::AttemptToStartServerTwice).into())
         }
     }
 }
@@ -80,13 +80,9 @@ where
     P: ServerProto<TcpStream>,
     S: NewService<Request = P::Request, Response = P::Response>,
     S::Instance: FiniteService,
-    Error: From<S::Error>
-        + From<<P::Transport as Stream>::Error>
-        + From<<P::Transport as Sink>::SinkError>
-        + From<P::Error>,
 {
     type Item = ListeningServer<S, P>;
-    type Error = Error;
+    type Error = AsyncServerError<S::Error, P::Error>;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         self.start_server()
